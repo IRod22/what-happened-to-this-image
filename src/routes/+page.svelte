905d501a9777cfeefd61cac3682261e7db0dccaa
IRod22 <ivan18rod@yahoo.com>
2023-02-17 16:24:46 -0600

    add basic functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    let before = ''
+    let after = ''
+</script>
+
+<h1>What Happened to This Image?</h1>
+
+<table>
+    <tr>
+        <th>Before</th>
+        <th></th>
+        <th>After</th>
+    </tr>
+    <tr>
+        <!-- svelte-ignore a11y-img-redundant-alt -->
+        <td><img src="{before}" alt="Original image"></td>
+        <td>&rarr;</td>
+        <!-- svelte-ignore a11y-img-redundant-alt -->
+        <td><img src="{after}" alt="Changed image"></td>
+    </tr>
+    <tr>
+        <td><label>URL: <input type="text" bind:value={before}></label></td>
+        <td></td>
+        <td><label>URL: <input type="text" bind:value={after}></label></td>
+    </tr>
+</table>
