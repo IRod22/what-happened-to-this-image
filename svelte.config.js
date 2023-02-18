027f779eb92b2b5a1ea4a076beabeeaed5c066b8
IRod22 <ivan18rod@yahoo.com>
2023-02-17 20:44:44 -0600

    fix svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,13 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
+		}),
 		paths: {
 		    base: process.env.NODE_ENV === "production" ? "/what-happened-to-this-image" : "",
 		},
