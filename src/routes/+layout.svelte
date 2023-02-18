c5d63abba26a60fb01a80648c7868227c5bbcf6d
IRod22 <ivan18rod@yahoo.com>
2023-02-18 13:29:58 -0600

    add usage instructions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,3 +10,25 @@
 </nav>
 
 <slot></slot>
+
+<section>
+    <h3>How do I use this?</h3>
+    <ol>
+        <li>
+            Paste the URL of the original image into the "URL" field under the "Original Image" area.
+            You should see the image within a few seconds at most.
+        </li>
+        <li>
+            Paste the URL of the original image into the "URL" field under the "Processed Image" area.
+            You should see the image within a few seconds at most.
+        </li>
+        <li>
+            Configure the image filters until the original image becomes the processed image.
+            You should be able to use your arrow keys to get fine-grained results.
+        </li>
+        <li>
+            See the immediate and fine-grained results.
+            Adjust the filters until the original image looks identical to the processed image.
+        </li>
+    </ol>
+</section>
